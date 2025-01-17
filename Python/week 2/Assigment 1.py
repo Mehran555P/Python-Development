c8a760647770db6cbf32fb5573a7aaa5785989f3
Mehran Hakimnia <hakimniamehran@gmail.com>
2025-01-17 21:50:20 +0330

    Start week 2

---
+++ Python/week 2/Assigment 1.py
@@ -0,0 +1,8 @@
+check1 = input("Is movie on the cinema?(y/n)")
+check2 = input("Do you have free time?(y/n)")
+check3 = input("Is ticket free?(y/n)")
+
+if (check1 == "y" and check2 == "y" and check3 == "y") :
+    print("You must go to the cinema.")
+else :
+    print("Plan is cancel.")
